aea040febf7a794609031dd1cdee3d0ab0475004
Ilya Batuev <il.batuev@yandex.ru>
2022-03-18 13:15:57 +0500

    7lesson

--- 1zadanie.py
+++ 1zadanie.py
@@ -1,21 +1,20 @@
-myfile = open(file="logs.txt", mode='r', encoding="utf-8")
+import os
+from os.path import join
+my_project = "my_project"
+settings = "settings"
+mainapp = "mainapp"
+adminapp = "adminapp"
+authapp = "authapp"
 
-for line in myfile:
-                remote_addr = line.split(" - - ")[0]
-                type_and_resource = line.split('"')[1]
-                request_type = type_and_resource.split()[0]
-                requested_resource = type_and_resource.split()[1]
-                print(f"'{remote_addr}', '{request_type}', '{requested_resource}'")
+path1 = join(".", my_project)
+os.makedirs(path1,exist_ok=True)
+path2 = join( my_project, settings)
+os.makedirs(path2,exist_ok=True)
+path3 = join( my_project, mainapp)
+os.makedirs(path3,exist_ok=True)
+path4 = join( my_project, adminapp)
+os.makedirs(path4,exist_ok=True)
+path5 = join( my_project, authapp)
+os.makedirs(path5,exist_ok=True)
 
 
-
-
-
-
-
-
-
-
-
-
-myfile.close()
--- 4zadanie.py
+++ 4zadanie.py
@@ -1,21 +1,18 @@
-from sys import argv
+import os
 
+directory = r'.'
+groups = [100, 1000, 10000, 100000]
+result = dict.fromkeys(groups, 0)
 
-if len(argv) < 2:
-    with open(file="bakery.csv", mode="r", encoding="utf-8") as bul_num:
-        for line in bul_num:
-            print(line)
-if len(argv) == 2:
-    i = int("".join(argv[1:]))
-    list_bul = []
-    with open(file="bakery.csv", mode="r", encoding="utf-8") as bul_num:
-        for line in bul_num:
-            list_bul.append(line)
-    print("".join(list_bul[i - 1:]))
-if len(argv) > 2:
-    i, l = "".join(argv[1:])
-    list_bul2 = []
-    with open(file="bakery.csv", mode="r", encoding="utf-8") as bul_num2:
-        for line in bul_num2:
-            list_bul2.append(line)
-    print("".join(list_bul2[int(i) - 1:int(l)]))
+for path_from_top, subdirs, files in os.walk(directory):
+    for file in files:
+        path = os.path.join(path_from_top, file)
+        size = os.path.getsize(path)
+        to_group = min(filter(lambda x: size < x, groups))
+        result[to_group] += 1
+
+b100 = result[100]
+b1000 = result[1000]
+b10000 = result[10000]
+b100000 = result[100000]
+print(f" {result} \n Тут {b100} файлов размером не более 100 байт;\n {b1000} файла больше 100 и не больше 1000;\n {b10000} файла больше 1000 и не больше 10000;\n И {b100000} файла больше 10000 и не больше 100000;")
