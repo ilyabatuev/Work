b4a489af32b1a2a2957232c6d0b4ed6cbadc02e7
Ilya Batuev <il.batuev@yandex.ru>
2022-03-19 16:08:48 +0500

    8lesson

--- 1zadanie.py
+++ 1zadanie.py
@@ -1,20 +1,9 @@
-import os
-from os.path import join
-my_project = "my_project"
-settings = "settings"
-mainapp = "mainapp"
-adminapp = "adminapp"
-authapp = "authapp"
-
-path1 = join(".", my_project)
-os.makedirs(path1,exist_ok=True)
-path2 = join( my_project, settings)
-os.makedirs(path2,exist_ok=True)
-path3 = join( my_project, mainapp)
-os.makedirs(path3,exist_ok=True)
-path4 = join( my_project, adminapp)
-os.makedirs(path4,exist_ok=True)
-path5 = join( my_project, authapp)
-os.makedirs(path5,exist_ok=True)
+import re
 
 
+
+def email_parse(email_address):
+    parsed = re.findall(r"([^@&]+)@([\w_-][\w_\.-]*\.[\w_-]{2,})$", email_address)
+    if not parsed:
+        raise ValueError(f"wrong email: {email_address}")
+    return dict(zip(["username", "domain"], parsed[0]))
--- 3zadanie.py
+++ 3zadanie.py
@@ -1,15 +1,28 @@
-import os
-import shutil
-root_dir=os.path.join('.')
-for root,dirs,files in os.walk(root_dir):
-    dst = os.path.join(root_dir, 'my_project', 'templates')
-    if 'templates' in root:
-        if len(dirs)!=0:
-            src=os.path.join(root,dirs[0])
-            dst=os.path.join(dst,dirs[0])
-            try:
-                shutil.copytree(src,dst,symlinks=False)
-            except FileExistsError:
-                continue
+from functools import wraps
 
 
+def type_logger(level=0):
+
+    def logger(func):
+
+        @wraps(func)
+        def decor(*argv):
+
+
+            if level > 0:
+
+                return 'calc_cube(' + ", ".join([f"{func(x)}:{type(func(x))}" for x in argv]) + ")"
+
+            else:
+
+                return "calc_cube(" + ", ".join([str(func(x)) for x in argv]) + ")"
+
+        return decor
+
+    return logger
+
+
+@ type_logger(2)
+def calc_cube(x):
+    """ cube of args """
+    return x ** 3
