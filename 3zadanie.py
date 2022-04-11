706ca397a671996ef15c1f65f0523b9d4b9b763c
Ilya Batuev <il.batuev@yandex.ru>
2022-04-11 15:15:03 +0500

    11lesson

--- 3zadanie.py
+++ 3zadanie.py
@@ -1,15 +1,25 @@
-import os
-import shutil
-root_dir=os.path.join('.')
-for root,dirs,files in os.walk(root_dir):
-    dst = os.path.join(root_dir, 'my_project', 'templates')
-    if 'templates' in root:
-        if len(dirs)!=0:
-            src=os.path.join(root,dirs[0])
-            dst=os.path.join(dst,dirs[0])
+class Error:
+    def __init__(self, *args):
+        self.my_list = []
+
+    def my_input(self):
+
+
+        while True:
             try:
-                shutil.copytree(src,dst,symlinks=False)
-            except FileExistsError:
-                continue
+                val = int(input('Введите значения и нажимайте Enter - '))
+                self.my_list.append(val)
+                print(f'Текущий список - {self.my_list} \n ')
+            except:
+                print(f"Недопустимое значение - строка и булево")
+                y_or_n = input(f'Попробовать еще раз? Y/N ')
 
+                if y_or_n == 'Y' or y_or_n == 'y':
+                    print(try_except.my_input())
+                elif y_or_n == 'N' or y_or_n == 'n':
+                    return f'Вы вышли'
+                else:
+                    return f'Вы вышли'
 
+try_except = Error(1)
+print(try_except.my_input())
--- 4zadanie.py
+++ 4zadanie.py
@@ -1,18 +1,22 @@
-import os
+class ComplexNumber:
+    def __init__(self, a, b, *args):
+        self.a = a
+        self.b = b
+        self.z = 'a + b * i'
 
-directory = r'.'
-groups = [100, 1000, 10000, 100000]
-result = dict.fromkeys(groups, 0)
+    def __add__(self, other):
+        print(f'Сумма z1 и z2 равна')
+        return f'z = {self.a + other.a} + {self.b + other.b} * i'
 
-for path_from_top, subdirs, files in os.walk(directory):
-    for file in files:
-        path = os.path.join(path_from_top, file)
-        size = os.path.getsize(path)
-        to_group = min(filter(lambda x: size < x, groups))
-        result[to_group] += 1
+    def __mul__(self, other):
+        print(f'Произведение z1 и z2 равно')
+        return f'z = {self.a * other.a - (self.b * other.b)} + {self.b * other.a} * i'
 
-b100 = result[100]
-b1000 = result[1000]
-b10000 = result[10000]
-b100000 = result[100000]
-print(f" {result} \n Тут {b100} файлов размером не более 100 байт;\n {b1000} файла больше 100 и не больше 1000;\n {b10000} файла больше 1000 и не больше 10000;\n И {b100000} файла больше 10000 и не больше 100000;")
+    def __str__(self):
+        return f'z = {self.a} + {self.b} * i'
+
+z_1 = ComplexNumber(1, -2)
+z_2 = ComplexNumber(3, 4)
+print(z_1)
+print(z_1 + z_2)
+print(z_1 * z_2)
